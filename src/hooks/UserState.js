dd29b71b4c8d92034eaf5acc7361172ae1e1cbcc
Sofia Calderon <sofia.ester.calderon@gmail.com>
2021-01-04 11:56:07 +1100

    removed auth

--- src/hooks/UserState.js
+++ src/hooks/UserState.js
@@ -16,25 +16,26 @@ const UserProvider = ({ children }) => {
 
   async function establishSession() {
     try {
-      const currentUser = await authHelper.getCurrentUser();
-      setUser({ session: true, adminMode: false });
-      if (
-        currentUser.signInUserSession.accessToken.payload[
-          "cognito:groups"
-        ].includes("admin")
-      ) {
-        console.log("admin user is current session");
-        setUser({ session: true, adminMode: true });
-      } else {
-        console.log("public user is current session");
-      }
+      // const currentUser = await authHelper.getCurrentUser();
+      // setUser({ session: true, adminMode: false });
+      // if (
+      //   currentUser.signInUserSession.accessToken.payload[
+      //     "cognito:groups"
+      //   ].includes("admin")
+      // ) {
+      //   console.log("admin user is current session");
+      //   setUser({ session: true, adminMode: true });
+      // } else {
+      //   console.log("public user is current session");
+      // }
+      setUser({ session: true, adminMode: true });
     } catch (e) {
       //login with def user
-      console.log("no current session, logging in with public user");
-      await authHelper.login(
-        process.env.REACT_APP_PUBLIC_USER_USERNAME,
-        process.env.REACT_APP_PUBLIC_USER_PASSWORD
-      );
+      // console.log("no current session, logging in with public user");
+      // await authHelper.login(
+      //   process.env.REACT_APP_PUBLIC_USER_USERNAME,
+      //   process.env.REACT_APP_PUBLIC_USER_PASSWORD
+      // );
       setUser({ session: true, adminMode: false });
     }
   }
